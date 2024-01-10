619917442897a635b11a56e61ad02cf0b2fd36c9
Kevin Kihneman <kihneman@gmail.com>
2024-01-10 17:33:07 -0600

    Add message key to router for user socket

--- pyguacd/router.py
+++ pyguacd/router.py
@@ -4,7 +4,9 @@ from os import makedirs
 import zmq
 import zmq.asyncio
 
-from .constants import GuacClientLogLevel, GUAC_CLIENT_ID_PREFIX, GUACD_SOCKET_DEFAULT_DIR, GUACD_ROUTER_SOCKET_PATH
+from .constants import (
+    GuacClientLogLevel, ZmqMsgKey, GUAC_CLIENT_ID_PREFIX, GUACD_SOCKET_DEFAULT_DIR, GUACD_ROUTER_SOCKET_PATH
+)
 from .libguac_wrapper import guac_parser_alloc, guac_parser_free, guac_socket_create_zmq, guac_socket_free
 from .log import guacd_log
 from .parser import parse_identifier
@@ -22,7 +24,10 @@ class Router:
         self.router_sock.bind(self.router_ipc_addr)
 
     async def zmq_listener(self):
-        user_ipc_addr = await self.router_sock.recv_string()
+        user_addr_key, user_ipc_addr = await self.router_sock.recv_multipart()
+        if user_addr_key != ZmqMsgKey.USER_ADDR:
+            print(f'Unexpected key for user socket "{user_addr_key}"')
+            return
 
         # This may need to be in another thread due to blocking libguac parser and libguac zmq
         guac_sock = guac_socket_create_zmq(zmq.PAIR, user_ipc_addr, False)
