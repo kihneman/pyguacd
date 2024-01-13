147fe69ef8f95762e20f054a2f7741477f06450a
Kevin Kihneman <kihneman@gmail.com>
2024-01-13 15:18:22 -0600

    Keep old connection working

--- pyguacd/connection.py
+++ pyguacd/connection.py
@@ -39,7 +39,7 @@ def guacd_add_user(proc: GuacdProc, parser, gsock, sock) -> int:
     #     data = user_socket.recv()
 
 
-def guacd_route_connection(gsock: POINTER(guac_socket), sock: socket.socket) -> int:
+def guacd_route_connection(sock: socket.socket, gsock: POINTER(guac_socket)) -> int:
     """Route a Guacamole connection
 
     Routes the connection on the given socket according to the Guacamole
@@ -81,12 +81,12 @@ def guacd_route_connection(gsock: POINTER(guac_socket), sock: socket.socket) -> 
         guacd_log(GuacClientLogLevel.GUAC_LOG_INFO, f'Creating new client for protocol "{identifier.decode()}"')
 
         # Create new client in the same process
-        # guacd_create_client(gsock, identifier)
+        guacd_create_client(gsock, identifier)
 
         # Create new process
-        proc = guacd_create_proc(identifier)
+        # proc = guacd_create_proc(identifier)
         new_process = 1
 
-    guacd_add_user(proc, parser_ptr, gsock, sock)
+    # guacd_add_user(proc, parser_ptr, gsock, sock)
     guac_parser_free(parser_ptr)
     return 0
