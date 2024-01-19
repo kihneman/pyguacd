3a7bbc1fed8a3bf26533fdd51f565003152b4518
Kevin Kihneman <kihneman@gmail.com>
2024-01-19 16:53:15 -0600

    Fix in and out addresses on zmq proxy

--- pyguacd/utils/zmq.py
+++ pyguacd/utils/zmq.py
@@ -29,7 +29,7 @@ def new_ipc_addr(base_path):
     return f'ipc://{base_path}{uid}'
 
 
-def start_zmq_client_proxy(base_path=None, pub_sub=False):
+def start_zmq_client_proxy(base_addr=None, pub_sub=False):
     """Connects users to existing clients for sending the user socket address"""
 
     if pub_sub:
@@ -38,16 +38,16 @@ def start_zmq_client_proxy(base_path=None, pub_sub=False):
         zmq_router_proxy = ThreadProxy(zmq.PAIR, zmq.PAIR)
 
     # Users connect to the in port
-    if base_path is None:
+    if base_addr is None:
         zmq_router_proxy.bind_in(f'ipc://{GUACD_ZMQ_PROXY_USER_SOCKET_PATH}')
     else:
-        zmq_router_proxy.bind_in(f'ipc://{base_path}-in')
+        zmq_router_proxy.bind_in(f'{base_addr}-in')
 
     # Clients connect to the out port
-    if base_path is None:
+    if base_addr is None:
         zmq_router_proxy.bind_out(f'ipc://{GUACD_ZMQ_PROXY_CLIENT_SOCKET_PATH}')
     else:
-        zmq_router_proxy.bind_out(f'ipc://{base_path}-out')
+        zmq_router_proxy.bind_out(f'{base_addr}-out')
 
     zmq_router_proxy.start()
     proxy_context = zmq_router_proxy.context_factory()
