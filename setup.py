f2d237305ef500bcec89896f8fcf0a767ad7fffa
Kevin Kihneman <kihneman@gmail.com>
2024-01-20 23:48:56 -0600

    Update setup.py entry point

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyguacd',
+    name='guacd',
     version='0.9.0',
     description='Keeper Guacamole Server Python SDK',
-    long_description='Python SDK for Keeper Guacamole Server',
+    long_description='Python launcher for Keeper Guacamole Server',
     author='Keeper Security',
     author_email='ops@keepersecurity.com',
-    packages=find_packages(),
+    packages=find_packages(exclude=['cython']),
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'pyguacd=pyguacd.__main__:main'
+            'guacd=guacd.daemon:main'
         ]
     }
 )
