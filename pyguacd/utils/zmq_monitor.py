54cf8df18579ced4bd2959843f876b36ad5370f7
Kevin Kihneman <kkihneman@keepersecurity.com>
2024-02-02 17:22:38 -0600

    Add functions to check expected events from ZeroMQ socket:
    - This allows for checking when a user connection
      is finished when handling a connection in the
      asyncio tcp server.
    - A user connection should have events for successfully
      connecting and disconnecting when identifier is parsed.
      Then the user connection should connect and disconnect
      again when the user is handled in the client process.

---
+++ pyguacd/utils/zmq_monitor.py
@@ -0,0 +1,53 @@
+from typing import Iterable, Optional
+
+import zmq
+import zmq.asyncio
+from zmq.utils.monitor import parse_monitor_message
+
+
+async def process_monitor_event(zmq_monitor: zmq.asyncio.Socket, expect_event: zmq.Event) -> Optional[str]:
+    """Wait for single ZeroMQ socket monitor event and check if the event matches expect_event or not.
+    Return an error message if event doesn't match.
+
+    :param zmq_monitor:
+        ZeroMQ Socket returned from the get_monitor_socket method of another ZeroMQ Socket object
+    :param expect_event:
+        The expected zmq.Event
+    :param msg:
+        The optional message to print if event matches
+    :return:
+        None if event matches, error message otherwise
+    """
+
+    # Wait for and parse the next monitor message
+    mon_msg = parse_monitor_message(await zmq_monitor.recv_multipart())
+
+    # Check the event key from the monitor message
+    event = mon_msg.get('event')
+    if event == expect_event:
+        # Return None if event matches
+        return None
+
+    else:
+        # Handle unexpected event error
+        event_name = event.name if isinstance(event, zmq.Event) else event
+        error_msg = f'Expected "{expect_event.name}" but got "{event_name}"'
+        return error_msg
+
+
+async def check_zmq_monitor_events(zmq_monitor: zmq.asyncio.Socket, zmq_events: Iterable[zmq.Event]) -> Optional[str]:
+    """Wait for multiple ZeroMQ socket events and return an error message if any event doesn't match what is expected.
+
+    :param zmq_monitor:
+        ZeroMQ Socket returned from the get_monitor_socket of another ZeroMQ Socket object
+    :param zmq_events:
+        An iterable of events that are expected
+    :return:
+        None if all events match, error message of unexpected event otherwise
+    """
+
+    for expect_event in zmq_events:
+        if error_msg := await process_monitor_event(zmq_monitor, expect_event) is not None:
+            return error_msg
+
+    return None
